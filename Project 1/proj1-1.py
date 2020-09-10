26129426d074f44cc90f5baa9a5ec5e2c9bd4de0
James Stevenson <jpatstevenson@gmail.com>
2020-09-10 11:19:37 -0700

    Add documentation

--- Project 1/proj1-1.py
+++ Project 1/proj1-1.py
@@ -1,6 +1,17 @@
 import sys
 
+# Class: CPSC 475
+# Team Member 1: James Stevenson
+# Submitted By: James Stevenson
+# GU Username: jstevenson4
+# File Name: proj1-1.py
+# Program finds the number of a given substring from within a given file
+# To Execute: python proj1-1.py
 
+
+# Pre: 
+# Post: Input file is opened and returned if inputted name is valid,
+#     otherwise input is requested indefinitely
 def my_open():
     while True:
         fin = input('Enter an input file name\n')
@@ -12,6 +23,9 @@ def my_open():
     return fin
 
 
+# Pre: string is string to be searched on, subStr is string being searched for
+#     and posStr_in is where the searching begins on 'string'
+# Post: True if subStr is in string starting at posStr_in, false otherwise
 def isSub(string, subStr, posStr_in):
     posSub = 0;
     posStr = posStr_in
@@ -24,6 +38,9 @@ def isSub(string, subStr, posStr_in):
     return True
 
 
+# Pre: string is a valid string to be searched on, subStr is a substring to be
+#     searched for
+# Post: Number of instances of subStr found in string is returned
 def find_number_of_subs(string, subStr):
     posStr = 0
     lastSub = len(string) - len(subStr)
@@ -38,6 +55,9 @@ def find_number_of_subs(string, subStr):
     return number_of_subs
 
 
+# Pre:
+# Post: User-defined file is opened then number of instances of user-defined substring is given
+#     within the text file
 def main():
     file_as_string = my_open().read()
     print(find_number_of_subs(file_as_string, input("Enter a substring\n")))
