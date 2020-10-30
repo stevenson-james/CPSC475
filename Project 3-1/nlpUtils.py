c8343d64418ce72b8cb87faa6411108beec3cf2d
James Stevenson <jpatstevenson@gmail.com>
2020-10-29 23:28:11 -0700

    Add comments

--- Project 3-1/nlpUtils.py
+++ Project 3-1/nlpUtils.py
@@ -1,8 +1,28 @@
+# Class: CPSC 475
+# Team Member 1: James Stevenson
+# Submitted By: James Stevenson
+# GU Username: jstevenson4
+# File Name: proj3-1.py
+# Description: Program gives functionality for tokenizing an input file and 
+#   generating n-grams from tokenized file
+# To Execute: import as a package in a main python script
+
+
+"""
+Returns list of each line in an input file
+Pre: fname is a valid string name of an input file
+Post: returns list of each line from input file
+"""
 def get_data(fname):
     f = open(fname, 'r', encoding='utf8')
     return [line for line in f]
 
 
+"""
+Tokenizes a list of lines from an input file
+Pre: text_lst is a list with each line as an item
+Post: returns tokenized list of sentences
+"""
 def tokenize(text_lst):
     new_text_lst = []
     for line in text_lst:
@@ -21,7 +41,12 @@ def tokenize(text_lst):
     return new_text_lst
 
 
-# gram_size > 0 and < 5
+"""
+Makes a list of n_grams from the sentence list with n being the gram_size
+Pre: sent_lst is a list with item a tokenized sentence, and gram_size is greater
+    than zero and less than five
+Post: returns a list of all n_grams of sent_lst
+"""
 def make_grams(sent_lst, gram_size):
     n_grams = []
     for line in sent_lst:
--- Project 3-1/proj3-1.py
+++ Project 3-1/proj3-1.py
@@ -1,7 +1,22 @@
 from nlpUtils import get_data, tokenize, make_grams
 from sys import argv
 
+# Class: CPSC 475
+# Team Member 1: James Stevenson
+# Submitted By: James Stevenson
+# GU Username: jstevenson4
+# File Name: proj3-1.py
+# Description: Program displays number of n-grams from user based on functions
+#   in nlpUtils.py
+# To Execute: python3 proj3-1.py <size of grams> <number of grams to display>
 
+
+"""
+Generates n-grams and displays given number of generated n-grams
+Pre: grams is a list of n-grams, num_grams is not greater
+    than number of n-grams in grams
+Post: prints n-grams from grams based on num_grams
+"""
 def display(grams, num_grams):
     for i in range(num_grams):
         print(grams[i])
